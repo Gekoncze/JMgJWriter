722c60f4d41e317f033db0b615ec0983e56f53ba
mg <gekoncze@seznam.cz>
2025-07-20 16:47:34 +0200

    added token writers + test

---
+++ src/cz/mg/java/writer/services/token/TokenWriters.java
@@ -0,0 +1,76 @@
+package cz.mg.java.writer.services.token;
+
+import cz.mg.annotations.classes.Service;
+import cz.mg.annotations.requirement.Mandatory;
+import cz.mg.java.writer.exceptions.WriterException;
+import cz.mg.token.Token;
+import cz.mg.token.tokens.*;
+import cz.mg.token.tokens.quote.DoubleQuoteToken;
+import cz.mg.token.tokens.quote.SingleQuoteToken;
+
+public @Service class TokenWriters {
+    private static volatile @Service TokenWriters instance;
+
+    public static @Service TokenWriters getInstance() {
+        if (instance == null) {
+            synchronized (Service.class) {
+                if (instance == null) {
+                    instance = new TokenWriters();
+                    instance.wordTokenWriter = WordTokenWriter.getInstance();
+                    instance.numberTokenWriter = NumberTokenWriter.getInstance();
+                    instance.symbolTokenWriter = SymbolTokenWriter.getInstance();
+                    instance.whitespaceTokenWriter = WhitespaceTokenWriter.getInstance();
+                    instance.doubleQuoteTokenWriter = DoubleQuoteTokenWriter.getInstance();
+                    instance.singleQuoteTokenWriter = SingleQuoteTokenWriter.getInstance();
+                    instance.commentTokenWriter = CommentTokenWriter.getInstance();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private @Service WordTokenWriter wordTokenWriter;
+    private @Service NumberTokenWriter numberTokenWriter;
+    private @Service SymbolTokenWriter symbolTokenWriter;
+    private @Service WhitespaceTokenWriter whitespaceTokenWriter;
+    private @Service DoubleQuoteTokenWriter doubleQuoteTokenWriter;
+    private @Service SingleQuoteTokenWriter singleQuoteTokenWriter;
+    private @Service CommentTokenWriter commentTokenWriter;
+
+    private TokenWriters() {
+    }
+
+    public @Mandatory String write(@Mandatory Token token) {
+        if (token instanceof WordToken wordToken) {
+            return wordTokenWriter.write(wordToken);
+        }
+
+        if (token instanceof NumberToken numberToken) {
+            return numberTokenWriter.write(numberToken);
+        }
+
+        if (token instanceof SymbolToken symbolToken) {
+            return symbolTokenWriter.write(symbolToken);
+        }
+
+        if (token instanceof WhitespaceToken whitespaceToken) {
+            return whitespaceTokenWriter.write(whitespaceToken);
+        }
+
+        if (token instanceof DoubleQuoteToken doubleQuoteToken) {
+            return doubleQuoteTokenWriter.write(doubleQuoteToken);
+        }
+
+        if (token instanceof SingleQuoteToken singleQuoteToken) {
+            return singleQuoteTokenWriter.write(singleQuoteToken);
+        }
+
+        if (token instanceof CommentToken commentToken) {
+            return commentTokenWriter.write(commentToken);
+        }
+
+        throw new WriterException(
+            "Unsupported token of type " + token.getClass().getSimpleName() + "."
+        );
+    }
+}
---
+++ test/cz/mg/java/writer/services/token/TokenWritersTest.java
@@ -0,0 +1,64 @@
+package cz.mg.java.writer.services.token;
+
+import cz.mg.annotations.classes.Service;
+import cz.mg.annotations.classes.Test;
+import cz.mg.java.writer.exceptions.WriterException;
+import cz.mg.test.Assert;
+import cz.mg.token.Token;
+import cz.mg.token.tokens.*;
+import cz.mg.token.tokens.quote.DoubleQuoteToken;
+import cz.mg.token.tokens.quote.SingleQuoteToken;
+
+public @Test class TokenWritersTest {
+    public static void main(String[] args) {
+        System.out.print("Running " + TokenWritersTest.class.getSimpleName() + " ... ");
+
+        TokenWritersTest test = new TokenWritersTest();
+        test.testWriteUnsupported();
+        test.testWriteWord();
+        test.testWriteNumber();
+        test.testWriteSymbol();
+        test.testWriteWhitespace();
+        test.testWriteDoubleQuote();
+        test.testWriteSingleQuote();
+        test.testWriteComment();
+
+        System.out.println("OK");
+    }
+
+    private final @Service TokenWriters writers = TokenWriters.getInstance();
+
+    private void testWriteUnsupported() {
+        Assert.assertThatCode(() -> writers.write(new Token("Hewwo", 0)))
+            .withMessage("Unsupported token as input should throw writer exception.")
+            .throwsException(WriterException.class);
+    }
+
+    private void testWriteWord() {
+        Assert.assertEquals("Hewwo", writers.write(new WordToken("Hewwo", 0)));
+    }
+
+    private void testWriteNumber() {
+        Assert.assertEquals("3.14f", writers.write(new NumberToken("3.14f", 0)));
+    }
+
+    private void testWriteSymbol() {
+        Assert.assertEquals("+=", writers.write(new SymbolToken("+=", 0)));
+    }
+
+    private void testWriteWhitespace() {
+        Assert.assertEquals("    ", writers.write(new WhitespaceToken("    ", 0)));
+    }
+
+    private void testWriteDoubleQuote() {
+        Assert.assertEquals("\"foo\\\"bar\"", writers.write(new DoubleQuoteToken("foo\"bar", 0)));
+    }
+
+    private void testWriteSingleQuote() {
+        Assert.assertEquals("'foo\\'bar'", writers.write(new SingleQuoteToken("foo'bar", 0)));
+    }
+
+    private void testWriteComment() {
+        Assert.assertEquals("/*todo*/", writers.write(new CommentToken("todo", 0)));
+    }
+}
