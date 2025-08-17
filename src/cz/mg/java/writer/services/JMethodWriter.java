364fb0c47b34ee3840c77a96a8c9255006db3002
mg <gekoncze@seznam.cz>
2025-08-17 22:16:43 +0200

    rewritten method writer

--- src/cz/mg/java/writer/services/JMethodWriter.java
+++ src/cz/mg/java/writer/services/JMethodWriter.java
@@ -5,11 +5,9 @@ import cz.mg.annotations.requirement.Mandatory;
 import cz.mg.annotations.requirement.Optional;
 import cz.mg.collections.components.StringJoiner;
 import cz.mg.collections.list.List;
-import cz.mg.java.entities.JAnnotation;
-import cz.mg.java.entities.JMethod;
-import cz.mg.java.entities.JType;
-import cz.mg.java.entities.JVariable;
+import cz.mg.java.entities.*;
 import cz.mg.java.entities.bounds.JBound;
+import cz.mg.java.writer.components.LineMerger;
 import cz.mg.java.writer.services.bounds.JBoundsWriter;
 import cz.mg.java.writer.services.formatting.Indentation;
 import cz.mg.java.writer.services.tokens.ExpressionWriter;
@@ -46,15 +44,14 @@ public @Service class JMethodWriter {
     private @Service JVariableWriter variableWriter;
     private @Service ExpressionWriter expressionWriter;
 
-    private JMethodWriter() {
+    protected JMethodWriter() {
     }
 
     public @Mandatory List<String> writeLines(@Mandatory JMethod method) {
         List<String> lines = new List<>();
         lines.addCollectionLast(writeComment(method.getComment()));
         lines.addCollectionLast(writeAnnotations(method.getAnnotations()));
-        lines.addCollectionLast(writeHeader(method));
-        writeImplementation(lines, method.getImplementation());
+        lines.addCollectionLast(writeCode(method));
         return lines;
     }
 
@@ -74,24 +71,23 @@ public @Service class JMethodWriter {
         return lines;
     }
 
-    private @Mandatory List<String> writeHeader(@Mandatory JMethod method) {
-        String modifiers = modifierWriter.write(method.getModifiers());
+    private @Mandatory List<String> writeCode(@Mandatory JMethod method) {
+        String modifiers = writeModifiers(method.getModifiers());
         String bounds = writeBounds(method.getBounds());
         String output = writeOutput(method.getOutput());
         String name = method.getName();
+        String header = writeHeader(modifiers, bounds, output, name);
+        List<String> parameters = writeParameters(header, method.getInput());
+        List<String> implementation = writeImplementation(method.getImplementation());
+        return new LineMerger()
+            .merge(new List<>(header))
+            .merge(parameters)
+            .merge(implementation)
+            .get();
+    }
 
-        String begin = new StringJoiner<>(modifiers, bounds, output, name)
-            .withDelimiter(" ")
-            .withFilter(s -> !s.isBlank())
-            .join();
-
-        List<String> input = writeInput(method.getInput());
-
-        if (input.isEmpty() || estimateLineLength(begin, input) < LIMIT) {
-            return writeHeaderWithParametersOnSingleLine(begin, input);
-        } else {
-            return writeHeaderWithParametersOnSeparateLines(begin, input);
-        }
+    private @Mandatory String writeModifiers(@Mandatory List<JModifier> modifiers) {
+        return modifierWriter.write(modifiers);
     }
 
     private @Mandatory String writeBounds(@Mandatory List<JBound> bounds) {
@@ -102,6 +98,42 @@ public @Service class JMethodWriter {
         return type == null ? "void" : typeWriter.write(type);
     }
 
+    private @Mandatory String writeHeader(@Mandatory String... parts) {
+        return new StringJoiner<>(parts)
+            .withDelimiter(" ")
+            .withFilter(s -> !s.isBlank())
+            .join();
+    }
+
+    private @Mandatory List<String> writeParameters(@Mandatory String header, @Mandatory List<JVariable> input) {
+        List<String> parameters = writeInput(input);
+        if (parameters.isEmpty() || estimateLineLength(header, parameters) < LIMIT) {
+            return writeParametersOnSingleLine(parameters);
+        } else {
+            return writeParametersOnSeparateLines(parameters);
+        }
+    }
+
+    private @Mandatory List<String> writeParametersOnSingleLine(@Mandatory List<String> parameters) {
+        String joinedInput = new StringJoiner<>(parameters)
+            .withDelimiter(", ")
+            .join();
+
+        return new List<>("(" + joinedInput + ")");
+    }
+
+    private @Mandatory List<String> writeParametersOnSeparateLines(@Mandatory List<String> parameters) {
+        List<String> lines = new List<>("(");
+        int i = 0;
+        for (String parameter : parameters) {
+            String delimiter = i < parameters.count() - 1 ? "," : "";
+            lines.addLast("    " + parameter + delimiter);
+            i++;
+        }
+        lines.addLast(")");
+        return lines;
+    }
+
     private @Mandatory List<String> writeInput(@Mandatory List<JVariable> input) {
         List<String> parameters = new List<>();
         for (JVariable parameter : input) {
@@ -110,51 +142,25 @@ public @Service class JMethodWriter {
         return parameters;
     }
 
-    private int estimateLineLength(@Mandatory String begin, @Mandatory List<String> input) {
-        int estimatedHeaderLength = Indentation.LENGTH + begin.length();
-        for (String parameter : input) {
+    private int estimateLineLength(@Mandatory String header, @Mandatory List<String> parameters) {
+        int estimatedHeaderLength = Indentation.LENGTH + header.length();
+        for (String parameter : parameters) {
             estimatedHeaderLength += parameter.length();
         }
-        estimatedHeaderLength += Math.max(0, 2 * (input.count() - 1)); // delimiters
+        estimatedHeaderLength += Math.max(0, 2 * (parameters.count() - 1)); // delimiters
         estimatedHeaderLength += 2 + 2; // brackets with space
         return estimatedHeaderLength;
     }
 
-    private @Mandatory List<String> writeHeaderWithParametersOnSingleLine(
-        @Mandatory String begin,
-        @Mandatory List<String> input
-    ) {
-        String joinedInput = new StringJoiner<>(input)
-            .withDelimiter(", ")
-            .join();
-
-        return new List<>(begin + "(" + joinedInput + ")");
-    }
-
-    private @Mandatory List<String> writeHeaderWithParametersOnSeparateLines(
-        @Mandatory String begin,
-        @Mandatory List<String> input
-    ) {
-        List<String> lines = new List<>(begin + "(");
-        int i = 0;
-        for (String parameter : input) {
-            String delimiter = i < input.count() - 1 ? "," : "";
-            lines.addLast("    " + parameter + delimiter);
-            i++;
-        }
-        lines.addLast(")");
-        return lines;
-    }
-
-    private void writeImplementation(@Mandatory List<String> lines, @Optional List<Token> implementation) {
+    private @Mandatory List<String> writeImplementation(@Optional List<Token> implementation) {
         if (implementation != null) {
-            // add curly brackets with implementation on separate line, to be improved later
-            lines.getLastItem().set(lines.getLast() + " {");
-            lines.addLast("    " + expressionWriter.write(implementation));
-            lines.addLast("}");
+            return new List<>(
+                " {",
+                "    " + expressionWriter.write(implementation),
+                "}"
+            );
         } else {
-            // no implementation, so only add semicolon
-            lines.getLastItem().set(lines.getLast() + ";");
+            return new List<>(";");
         }
     }
 }
